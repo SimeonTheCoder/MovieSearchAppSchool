a0d6e44b9f993f2f867422fc8e8e5f74dcd5a079
Simeon Petkov <petkov.moni@gmail.com>
2025-04-09 14:30:04 +0300

    Favorite movies base implementation

--- script.js
+++ script.js
@@ -1,6 +1,70 @@
 const BASE_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=8106045&t=';
 
+let favorites = [];
+
+if (localStorage.getItem('favoritesMovies') != null) {
+    favorites = JSON.parse(localStorage.getItem('favoritesMovies'));
+
+    readFavoriteMovies();
+}
+
 let obj;
+let counter = favorites.length;
+
+function renderFavoriteMovie(currMovie, id) {
+    let baseDiv = document.getElementById('fave-mvs');
+
+    let div = document.createElement('div');
+    div.classList.add('container-favorites');
+
+    let poster = document.createElement('img');
+    poster.src = currMovie.Poster;
+
+    let div2 = document.createElement('div');
+    div2.style.display = 'inline';
+    div2.style.width = '90%';
+
+    let filmTitle = document.createElement('h3');
+    filmTitle.innerHTML = currMovie.Title;
+    filmTitle.style.margin = '10px';
+    filmTitle.style.fontSize = '24px';
+
+    let btnRemove = document.createElement('button');
+    btnRemove.textContent = 'Remove';
+    btnRemove.id = 'remove' + id;
+    btnRemove.style.margin = '0px';
+    btnRemove.addEventListener("click", removeFromFavorites);
+
+    let btnView = document.createElement('button');
+    btnView.textContent = 'View';
+    btnView.style.margin = '0px';
+
+    div2.appendChild(filmTitle);
+
+    div2.appendChild(btnRemove);
+    div2.appendChild(btnView);
+
+    div.appendChild(poster);
+    div.appendChild(div2);
+
+    baseDiv.appendChild(div);
+}
+
+function readFavoriteMovies() {
+    // <div class="container-favorites" id="film-info">
+    //     <img id="film-poster" src="https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg">
+        
+    //     <div style="display: inline; width: 90%;">
+    //         <h3 id="film-info-fave-title1">AEUUEH</h3>
+    //     </div>  
+    // </div>
+    let baseDiv = document.getElementById('fave-mvs');
+
+    let favePrev = baseDiv.childNodes;
+
+    for (let i = 0; i < favorites.length; i ++)
+        renderFavoriteMovie(favorites[i], i);
+}
 
 async function submitData() {
     let content = document.getElementById('film-title').value;
@@ -18,8 +82,6 @@ async function visualiseData() {
     
     await submitData();
 
-    console.log(obj);
-
     document.getElementById('film-info-title').innerHTML = obj.Title;
     document.getElementById('film-director').innerHTML = obj.Director;
     document.getElementById('film-year').innerHTML = obj.Year;
@@ -28,4 +90,21 @@ async function visualiseData() {
     document.getElementById('film-actors').innerHTML = obj.Actors;
 
     document.getElementById('film-poster').src = obj.Poster;
+}
+
+function addToFavorites() {
+    favorites.push(obj);
+
+    localStorage.setItem("favoritesMovies", JSON.stringify(favorites));
+
+    renderFavoriteMovie(counter++);
+}
+
+function removeFromFavorites(e) {
+    let index = favorites[e.target.id.substring(6)];
+    console.log(index);
+
+    favorites.splice(index, 1);
+
+    localStorage.setItem("favoritesMovies", JSON.stringify(favorites));
 }
